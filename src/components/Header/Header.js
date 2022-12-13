d8726b68d0f0f7081a6a5e7c4adc1c5c384e3ae4
Apatira Temitope <83637849+Temitope-Apatira@users.noreply.github.com>
2022-12-13 18:48:38 +0100

    feat: header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import { Box, Flex, Image } from "@chakra-ui/react";
+import logo from "../../assets/images/veergelogo.png";
+import React from "react";
+export const Header = () => {
+  return (
+    <Flex
+      maxW="1740px"
+      m="0 auto"
+      w="100%"
+      px="78px"
+      height="82px"
+      position="fixed"
+      zIndex="1"
+      bg="#191919"
+      align="center"
+      boxShadow="0px 4px 8px rgba(0, 0, 0, 0.02)"
+    >
+      <Image src={logo} />
+    </Flex>
+  );
+};
