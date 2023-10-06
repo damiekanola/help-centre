a253a9cc0459c90049319754fbb97f5ba8cb9cef
Waris <oloyedewaris@gmail.com>
2023-10-06 11:01:10 +0100

    feature: blog homepage

---
+++ src/constant/blog.js
@@ -0,0 +1,192 @@
+import new_era from '../assets/images/blog/new_era.png';
+import a_ticketing_system from '../assets/images/blog/a_ticketing_system.png';
+import fractional_ownership from '../assets/images/blog/fractional_ownership.png';
+import inventory_management from '../assets/images/blog/inventory_management.png';
+import lead_management from '../assets/images/blog/lead_management.png';
+import letter_from_CEO from '../assets/images/blog/letter_from_CEO.png';
+import revenue_recognition from '../assets/images/blog/revenue_recognition.png';
+import smart_payment from '../assets/images/blog/smart_payment.png';
+import veergeNDdata from '../assets/images/blog/veerge&data.png';
+import why_veerge from '../assets/images/blog/why_veerge.png';
+
+export const blogDataRead = [
+    {
+        img: new_era,
+        title: 'New era of Real Estate',
+        type: 'Peaks',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: why_veerge,
+        title: 'Why Veerge instead of building',
+        type: 'Peaks',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: veergeNDdata,
+        title: 'Veerge & Data analytics',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: fractional_ownership,
+        title: 'Fractional ownership',
+        type: 'Peaks',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: fractional_ownership,
+        title: 'Understanding fractional real estate',
+        type: 'Industry Insight',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: letter_from_CEO,
+        title: 'A letter from CEO',
+        type: 'Culture',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: lead_management,
+        title: 'Lead management system',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: a_ticketing_system,
+        title: 'A ticketing system serves as...',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: inventory_management,
+        title: 'Inventory management system',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: revenue_recognition,
+        title: 'Revenue recognition solution',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: smart_payment,
+        title: 'Smart payment plan',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+]
+
+export const blogDataRecent = [
+    {
+        img: smart_payment,
+        title: 'Smart payment plan',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: veergeNDdata,
+        title: 'Veerge & Data analytics',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: revenue_recognition,
+        title: 'Revenue recognition solution',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: lead_management,
+        title: 'Lead management system',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: a_ticketing_system,
+        title: 'A ticketing system serves as...',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: inventory_management,
+        title: 'Inventory management system',
+        type: 'Features',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: new_era,
+        title: 'New era of Real Estate',
+        type: 'Peaks',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: why_veerge,
+        title: 'Why Veerge instead of building',
+        type: 'Peaks',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: fractional_ownership,
+        title: 'Fractional ownership',
+        type: 'Peaks',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: fractional_ownership,
+        title: 'Understanding fractional real estate',
+        type: 'Industry Insight',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+    {
+        img: letter_from_CEO,
+        title: 'A letter from CEO',
+        type: 'Culture',
+        readTime: '5mins read',
+        datePosted: 'Jul 5, 2023',
+        path: '/new_era'
+    },
+]
---
+++ src/pages/Blog/Blog.js
@@ -0,0 +1,145 @@
+import { Box, Center, Flex, HStack, Image, Text } from '@chakra-ui/react'
+import React, { useRef, useState } from 'react'
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom'
+import timeIcon from '../../assets/icons/time_icon.svg'
+import { blogDataRead, blogDataRecent } from '../../constant/blog';
+import { handleIconType } from '../../utils/handleIconType';
+import { MdOutlineArrowRightAlt } from 'react-icons/md';
+import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
+
+export const Blog = () => {
+  const readScollToRef = useRef();
+
+  return (
+    <Box w='full' pt='250px' pb='50px'>
+      <Box w='85%' mx='auto'>
+        <Flex borderBottom={'1px solid #CBCBCB'} w='full' px='19px' align={'center'} justify={'space-between'} mb='30px'>
+          <Text fontSize={'36px'} fontWeight={600} mb='20px' color='#191919'>Most Read</Text>
+          <HStack spacing={'15px'}>
+            <Center
+              cursor={'pointer'}
+              onClick={() => readScollToRef.current.scrollTo(0, 1000)}
+              h='36px' w='36px' borderRadius={'full'}
+              color='#191919' border={'2px solid #191919'}
+            >
+              <SlArrowLeft fontWeight={700} />
+            </Center>
+            <Center
+              cursor={'pointer'}
+              onClick={() => readScollToRef.current.scrollTo(0, 1000)}
+              h='36px' w='36px' borderRadius={'full'}
+              color='#191919' border={'2px solid #191919'}
+            >
+              <SlArrowRight fontWeight={700} />
+            </Center>
+          </HStack>
+        </Flex>
+        <HStack ref={readScollToRef} px='20px' spacing='45px' alignItems={'center'} h='470px' overflowX={'scroll'} overflowY={'hidden'} className='hide_scroll'>
+          {blogDataRead.map(detail => (
+            <Link to={detail.path || ''}>
+              <Box
+                h={'432px'}
+                borderRadius={'16px'}
+                shadow={'md'}
+                as={motion.div}
+                mx={'auto'}
+                w='272px'
+                cursor={"pointer"}
+                whileHover={{ scale: 1.02 }}
+                whileTap={{ scale: 0.98 }}
+              >
+                <Image
+                  borderTopRightRadius={'16px'}
+                  borderTopLeftRadius={'16px'}
+                  w='full' src={detail.img}
+                  h='272.2px'
+                />
+                <Box px='16px' py='16px'>
+                  <HStack spacing={'8px'} align={'center'} fontSize={'10px'}>
+                    <Flex align={'center'} gap={'4px'} py='2px' px='8px' borderRadius={'4px'} border='1px solid rgba(69, 69, 254, 0.10)'>
+                      {handleIconType(detail.type)}
+                      <Text fontSize={'10px'} fontWeight='400'>{detail.type}</Text>
+                    </Flex>
+                    <Flex align={'center'} gap={'4px'} py='2px' px='8px' borderRadius={'4px'} border='1px solid rgba(69, 69, 254, 0.10)'>
+                      <Image w='12px' h='12px' src={timeIcon} />
+                      <Text fontSize={'10px'} fontWeight='400'>{detail.datePosted}</Text>
+                    </Flex>
+                  </HStack>
+                  <Text my='10px' fontSize={'20px'} fontWeight={600} color={'#101828'}>{detail.title}</Text>
+                  <Box w='fit-content' bg='#4545FE1A' borderRadius='full' px='8px' py='2px'>
+                    <Text fontSize={'10px'} fontWeight={300} color='#4545FE' mx='auto'>{detail.readTime}</Text>
+                  </Box>
+                </Box>
+              </Box>
+            </Link>
+          ))}
+        </HStack>
+      </Box>
+      <Box w='85%' mx='auto' mt='44px'>
+        <Flex borderBottom={'1px solid #CBCBCB'} w='full' px='19px' align={'center'} justify={'space-between'} mb='30px'>
+          <Text fontSize={'36px'} fontWeight={600} mb='20px' color='#191919'>Most Recent</Text>
+          <HStack spacing={'15px'}>
+            <Center
+              cursor={'pointer'}
+              onClick={() => readScollToRef.current.scrollTo(0, 1000)}
+              h='36px' w='36px' borderRadius={'full'}
+              color='#191919' border={'2px solid #191919'}
+            >
+              <SlArrowLeft fontWeight={700} />
+            </Center>
+            <Center
+              cursor={'pointer'}
+              onClick={() => readScollToRef.current.scrollTo(0, 1000)}
+              h='36px' w='36px' borderRadius={'full'}
+              color='#191919' border={'2px solid #191919'}
+            >
+              <SlArrowRight fontWeight={700} />
+            </Center>
+          </HStack>
+        </Flex>
+        <HStack ref={readScollToRef} px='20px' spacing='45px' alignItems={'center'} h='470px' overflowX={'scroll'} overflowY={'hidden'} className='hide_scroll'>
+          {blogDataRecent.map(detail => (
+            <Link to={detail.path || ''}>
+              <Box
+                h={'432px'}
+                borderRadius={'16px'}
+                shadow={'md'}
+                as={motion.div}
+                mx={'auto'}
+                w='272px'
+                cursor={"pointer"}
+                whileHover={{ scale: 1.02 }}
+                whileTap={{ scale: 0.98 }}
+              >
+                <Image
+                  borderTopRightRadius={'16px'}
+                  borderTopLeftRadius={'16px'}
+                  w='full' src={detail.img}
+                  h='272.2px'
+                />
+                <Box px='16px' py='16px'>
+                  <HStack spacing={'8px'} align={'center'} fontSize={'10px'}>
+                    <Flex align={'center'} gap={'4px'} py='2px' px='8px' borderRadius={'4px'} border='1px solid rgba(69, 69, 254, 0.10)'>
+                      {handleIconType(detail.type)}
+                      <Text fontSize={'10px'} fontWeight='400'>{detail.type}</Text>
+                    </Flex>
+                    <Flex align={'center'} gap={'4px'} py='2px' px='8px' borderRadius={'4px'} border='1px solid rgba(69, 69, 254, 0.10)'>
+                      <Image w='12px' h='12px' src={timeIcon} />
+                      <Text fontSize={'10px'} fontWeight='400'>{detail.datePosted}</Text>
+                    </Flex>
+                  </HStack>
+                  <Text my='10px' fontSize={'20px'} fontWeight={600} color={'#101828'}>{detail.title}</Text>
+                  <Box w='fit-content' bg='#4545FE1A' borderRadius='full' px='8px' py='2px'>
+                    <Text fontSize={'10px'} fontWeight={300} color='#4545FE' mx='auto'>{detail.readTime}</Text>
+                  </Box>
+                </Box>
+              </Box>
+            </Link>
+          ))}
+        </HStack>
+      </Box>
+    </Box>
+  )
+}
+
